a413ab6cd1b62d499599a787ca517a7e50c74f2c
wonderlandamy <99461657+wonderlandamy@users.noreply.github.com>
2025-12-12 10:49:05 +0800

    Delete data directory

--- data/__init__.py
+++
@@ -1,3 +0,0 @@
-# encoding: utf-8
-
-from .build import make_data_loader
--- data/samplers/__init__.py
+++
@@ -1,3 +0,0 @@
-# encoding: utf-8
-
-from .triplet_sampler import RandomIdentitySampler, RandomIdentitySampler_alignedreid   
